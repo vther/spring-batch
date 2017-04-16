74b3f01b8676a9544756238c16ddc27e18975b6c
vther <vther@qq.com>
2017-04-16 21:42:00 +0800

    提交spring batch学习代码

--- spring-batch-example/src/main/java/com/vther/spring/batch/ch07/jms/JMSDetectItemWriteListener.java
+++ spring-batch-example/src/main/java/com/vther/spring/batch/ch07/jms/JMSDetectItemWriteListener.java
@@ -1,42 +1,35 @@
-/**
- * 
- */
 package com.vther.spring.batch.ch07.jms;
 
 import com.vther.spring.batch.ch07.CreditBill;
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.jms.core.JmsTemplate;
 
-/**
- * @author bruce.liu(mailto:jxta.liu@gmail.com)
- * 2013-9-21下午08:51:51
- * @param <S>
- */
-public class JMSDetectItemWriteListener implements StepExecutionListener{
-	private JmsTemplate jmsTemplate;
-	
-	@Override
-	public void beforeStep(StepExecution stepExecution) {}
 
-	@Override
-	public ExitStatus afterStep(StepExecution stepExecution) {
-		int writeCount = 0;
-		Object obj = jmsTemplate.receiveAndConvert();
-		while(null != obj){
-			writeCount++;
-			CreditBill result = (CreditBill) obj;
-			System.out.println("Receive from jms queue:"+result);
-			obj = jmsTemplate.receiveAndConvert();
-		}
-		Assert.assertEquals(stepExecution.getWriteCount(), writeCount);
-		return stepExecution.getExitStatus();
-	}
+public class JMSDetectItemWriteListener implements StepExecutionListener {
+    private JmsTemplate jmsTemplate;
 
-	public void setJmsTemplate(JmsTemplate jmsTemplate) {
-		this.jmsTemplate = jmsTemplate;
-	}
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        int writeCount = 0;
+        Object obj = jmsTemplate.receiveAndConvert();
+        while (null != obj) {
+            writeCount++;
+            CreditBill result = (CreditBill) obj;
+            System.out.println("Receive from jms queue:" + result);
+            obj = jmsTemplate.receiveAndConvert();
+        }
+        Assert.assertEquals(stepExecution.getWriteCount(), writeCount);
+        return stepExecution.getExitStatus();
+    }
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
 }
